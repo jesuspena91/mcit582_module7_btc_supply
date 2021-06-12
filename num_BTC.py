2be4c84e80f4ea4444ad4be7c3e7c633b82b5cc3
jesuspena91 <jesusp@seas.upenn.edu>
2021-06-12 17:10:21 -0500

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -5,7 +5,7 @@ def num_BTC(b):
   block_level = 1
   current_reward = float(50)
   halving_event = 0
-  total_reward = 0
+  total_reward = float(0)
 
   while block_level <= b:
 
@@ -17,3 +17,5 @@ def num_BTC(b):
 
     total_reward = total_reward + current_reward
     block_level = block_level + 1
+
+  return total_reward

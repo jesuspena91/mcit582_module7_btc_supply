74d566f76604ca078c33caab3c21bf7c64b9495d
jesuspena91 <jesusp@seas.upenn.edu>
2021-06-12 17:09:16 -0500

    Create num_BTC.py

---
+++ num_BTC.py
@@ -0,0 +1,19 @@
+import math
+
+def num_BTC(b):
+
+  block_level = 1
+  current_reward = float(50)
+  halving_event = 0
+  total_reward = 0
+
+  while block_level <= b:
+
+    if halving_event < 210000:
+      halving_event = halving_event + 1
+    else:
+      current_reward = float(current_reward/2)
+      halving_event = 1
+
+    total_reward = total_reward + current_reward
+    block_level = block_level + 1
